42c2e57d44eaecb9e63254be1305847bc47fb4c6
Hans Deragon <hans@deragon.biz>
2022-12-10 20:53:50 -0500

    Change 'hdgpull' to list branches without verbose.

--- .env/env.gen.devel.sh
+++ .env/env.gen.devel.sh
@@ -60,7 +60,7 @@ fi
 # Git
 which git >/dev/null 2>&1
 if (( $? == 0 )); then
-  alias hdgpull='git pull --all --prune; git branch -avv'
+  alias hdgpull='git pull --all --prune; git branch -a'
   alias hdgstatus='git status --untracked-files=no'
   alias hdgitgvim='git difftool --tool=hdgvimdiff --no-prompt &'
   alias cdgroot='cdprint "$(git rev-parse --show-toplevel)"'  # Return root directory of current Git repo.
