a2c449315f3f60014b7c26a15b7b1ae8145960e5
Hans Deragon <hans@deragon.biz>
2024-12-14 10:08:28 -0500

    Reorganise comment about nvim-treesitter.
    Aesthetic changes only.  No logic changed.

--- .env/vim/hdinit.lua
+++ .env/vim/hdinit.lua
@@ -33,13 +33,13 @@ require 'lspconfig'.pyright.setup{}
 -- nvim-treesitter - https://github.com/nvim-treesitter/nvim-treesitter
 -- ────────────────────────────────────────────────────────────────────────────
 --
---  The goal of nvim-treesitter is both to provide a simple and easy way to
---  use the interface for tree-sitter in Neovim and to provide some basic
---  functionality such as highlighting based on it.
---
 --  Tree-sitter is a parser generator tool and an incremental parsing library.
 --  It can build a concrete syntax tree for a source file and efficiently
 --  update the syntax tree as the source file is edited.
+--
+--  The goal of nvim-treesitter is both to provide a simple and easy way to
+--  use the interface for tree-sitter in Neovim and to provide some basic
+--  functionality such as highlighting based on it.
 local ts = require 'nvim-treesitter.configs'
 ts.setup {
   -- List of languages supported out-of-the-box at:
