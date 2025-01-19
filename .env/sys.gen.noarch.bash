a9000122e82b36289300dd53312efa57b9eac2a5
Hans Deragon <hans@deragon.biz>
2025-01-19 17:56:14 -0500

    Add alias 'hdxa'.
    Alias 'hdxa' calls xargs with '\n' as the delimiter.  Thus one perform:
    
      cat "files.txt" | hdxa touch

--- .env/sys.gen.noarch.bash
+++ .env/sys.gen.noarch.bash
@@ -670,6 +670,7 @@ alias hd-iso8859-12utf='recode -v ISO-8859-1..UTF-8'
 alias addcurpath='export PATH=`pwd`:$PATH;export PATH="$("${HDENVDIR}/bin/minpath" "${PATH}")"'
 alias hup='killall -HUP'
 alias surf='hd-decompose'
+alias hdxa='xargs -d"\n"'
 
 type nmap >/dev/null 2>&1 && \
   alias netosdetect='nmap -A -T aggressive -p "[-65535]" -P0'
