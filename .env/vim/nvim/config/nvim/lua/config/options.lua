ca931737b6f75b316eb547fba88f5225cb3b7fa2
Hans Deragon <hans@deragon.biz>
2025-10-11 11:17:23 -0400

    Set mapleader to '\'.

--- .env/vim/nvim/config/nvim/lua/config/options.lua
+++ .env/vim/nvim/config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Added by Hans Deragon (hderagon@lacaisse.com), 2025-10-16 15:19:49 EDT
+-- mapleader defines the global leader key, used for general mappings.
+vim.g.mapleader = "\\"
+
+-- Added by Hans Deragon (hderagon@lacaisse.com), 2025-10-16 15:19:49 EDT
+-- maplocalleader defines the local leader key, intended for buffer-specific
+-- or filetype-specific mappings. In most cases, it's convenient to set them
+-- to the same key.
+vim.g.maplocalleader = "\\"
